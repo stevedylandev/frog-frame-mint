916e191cbac0a0247fa3bddbe113f816ff96d584
Steve <stevedsimkins@gmail.com>
2024-03-05 16:25:10 -0500

    WIP

--- app/api/[[...routes]]/route.tsx
+++ app/api/[[...routes]]/route.tsx
@@ -2,6 +2,7 @@
 
 import { Button, Frog, TextInput } from 'frog'
 import { handle } from 'frog/next'
+import { abi } from './abi.json'
 
 const app = new Frog({
   assetsPath: '/',
@@ -13,7 +14,7 @@ const app = new Frog({
 // Uncomment to use Edge Runtime
 // export const runtime = 'edge'
 
-app.frame('/', (c) => {
+/* app.frame('/', (c) => {
   const { buttonValue, inputText, status } = c
   const fruit = inputText || buttonValue
   return c.res({
@@ -60,6 +61,55 @@ app.frame('/', (c) => {
       <Button value="bananas">Bananas</Button>,
       status === 'response' && <Button.Reset>Reset</Button.Reset>,
     ],
+  })
+}) */
+
+app.frame('/', (c) => {
+  return c.res({
+    action: '/finish',
+    image: (
+      <div style={{ color: 'white', display: 'flex', fontSize: 60 }}>
+        Perform a transaction
+      </div>
+    ),
+    intents: [
+      <TextInput placeholder="Value (ETH)" />,
+      <Button.Transaction target="/send-ether">Send Ether</Button.Transaction>,
+      <Button.Transaction target="/mint">Mint</Button.Transaction>,
+    ]
+  })
+})
+ 
+app.frame('/finish', (c) => {
+  const { transactionId } = c
+  return c.res({
+    image: (
+      <div style={{ color: 'white', display: 'flex', fontSize: 60 }}>
+        Transaction ID: {transactionId}
+      </div>
+    )
+  })
+})
+ 
+/* app.transaction('/send-ether', (c) => {
+  const { inputText } = c
+  // Send transaction response.
+  return c.send({
+    chainId: 'eip155:1',
+    to: '0xd2135CfB216b74109775236E36d4b433F1DF507B',
+    value: parseEther(inputText),
+  })
+}) */
+ 
+app.transaction('/mint', (c) => {
+  const { inputText } = c
+  // Contract transaction response.
+  return c.contract({
+    abi,
+    chainId: 'eip155:1',
+    functionName: 'mint',
+    args: ["0xaD73eafCAc4F4c6755DFc61770875fb8B6bC8A25", "ipfs://CID"],
+    to: '0x83117b1eB0A3d88b5cc4DCaFaF8f7c7c0B0164Ff',
   })
 })
 
