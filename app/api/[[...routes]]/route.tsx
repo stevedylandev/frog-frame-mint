52211df06bc2c8c491121a74a5be2bb3d67afc76
Steve <stevedsimkins@gmail.com>
2024-03-07 16:05:53 -0500

    update test

--- app/api/[[...routes]]/route.tsx
+++ app/api/[[...routes]]/route.tsx
@@ -1,6 +1,6 @@
 /** @jsxImportSource frog/jsx */
 
-import { Button, Frog, TextInput } from 'frog'
+import { Button, Frog } from 'frog'
 import { handle } from 'frog/next'
 import { abi } from './abi.json'
 
@@ -11,58 +11,6 @@ const app = new Frog({
   // hubApiUrl: 'https://api.hub.wevm.dev',
 })
 
-// Uncomment to use Edge Runtime
-// export const runtime = 'edge'
-
-/* app.frame('/', (c) => {
-  const { buttonValue, inputText, status } = c
-  const fruit = inputText || buttonValue
-  return c.res({
-    image: (
-      <div
-        style={{
-          alignItems: 'center',
-          background:
-            status === 'response'
-              ? 'linear-gradient(to right, #432889, #17101F)'
-              : 'black',
-          backgroundSize: '100% 100%',
-          display: 'flex',
-          flexDirection: 'column',
-          flexWrap: 'nowrap',
-          height: '100%',
-          justifyContent: 'center',
-          textAlign: 'center',
-          width: '100%',
-        }}
-      >
-        <div
-          style={{
-            color: 'white',
-            fontSize: 60,
-            fontStyle: 'normal',
-            letterSpacing: '-0.025em',
-            lineHeight: 1.4,
-            marginTop: 30,
-            padding: '0 120px',
-            whiteSpace: 'pre-wrap',
-          }}
-        >
-          {status === 'response'
-            ? `Nice choice.${fruit ? ` ${fruit.toUpperCase()}!!` : ''}`
-            : 'Welcome!'}
-        </div>
-      </div>
-    ),
-    intents: [
-      <TextInput placeholder="Enter custom fruit..." />,
-      <Button value="apples">Apples</Button>,
-      <Button value="oranges">Oranges</Button>,
-      <Button value="bananas">Bananas</Button>,
-      status === 'response' && <Button.Reset>Reset</Button.Reset>,
-    ],
-  })
-}) */
 
 app.frame('/', (c) => {
   return c.res({
@@ -73,8 +21,6 @@ app.frame('/', (c) => {
       </div>
     ),
     intents: [
-      <TextInput placeholder="Value (ETH)" />,
-      <Button.Transaction target="/send-ether">Send Ether</Button.Transaction>,
       <Button.Transaction target="/mint">Mint</Button.Transaction>,
     ]
   })
@@ -91,25 +37,14 @@ app.frame('/finish', (c) => {
   })
 })
  
-/* app.transaction('/send-ether', (c) => {
-  const { inputText } = c
-  // Send transaction response.
-  return c.send({
-    chainId: 'eip155:1',
-    to: '0xd2135CfB216b74109775236E36d4b433F1DF507B',
-    value: parseEther(inputText),
-  })
-}) */
  
 app.transaction('/mint', (c) => {
-  const { inputText } = c
-  // Contract transaction response.
   return c.contract({
     abi,
-    chainId: 'eip155:1',
+    chainId: 'eip155:84532',
     functionName: 'mint',
-    args: ["0xaD73eafCAc4F4c6755DFc61770875fb8B6bC8A25", "ipfs://CID"],
-    to: '0x83117b1eB0A3d88b5cc4DCaFaF8f7c7c0B0164Ff',
+    args: ["0xaD73eafCAc4F4c6755DFc61770875fb8B6bC8A25"],
+    to: '0x893af2c848edc36f95d0fbd85a409c3191ddb0b8',
   })
 })
 
